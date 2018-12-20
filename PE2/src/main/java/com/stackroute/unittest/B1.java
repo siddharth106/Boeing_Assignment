1f643c527fcedcb2cdc719ac65056503e1c38ae0
siddharth106 <siddharthagarwal177@h=gmail.com>
2018-12-20 11:10:35 +0530

    PE2

---
+++ PE2/src/main/java/com/stackroute/unittest/B1.java
@@ -0,0 +1,9 @@
+package com.stackroute.unittest;
+
+public class B1 {
+
+    public static String isPalin(String s){
+        return "dfdf";
+
+    }
+}
---
+++ PE2/src/main/test/com/stackroute/unittest/B1Test.java
@@ -0,0 +1,14 @@
+package com.stackroute.unittest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class B1Test {
+
+
+    @Test
+    public void isPalin1() {
+        assertEquals("ffd",B1.isPalin("dff"));
+    }
+}
